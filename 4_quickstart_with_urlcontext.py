1611b3d082dbcd1a42ed0be89db8c6cccd146de4
Ryan Swift <ryan@mlh.io>
2025-09-29 16:54:53 -0400

    generalized 4

--- 4_quickstart_with_urlcontext.py
+++ 4_quickstart_with_urlcontext.py
@@ -13,7 +13,7 @@ client = genai.Client(api_key=os.environ["GOOGLE_GEMINI_API_KEY"])
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="When is hackumbc?",
+    contents="When is {HACKATHON_NAME}?",
     config=types.GenerateContentConfig(
         thinking_config=types.ThinkingConfig(thinking_budget=0), # Disables thinking
         #tools=tools, # Uncomment to pass your tools (defined above) to the config
